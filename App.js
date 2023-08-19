eb42401c9b224dc245c31f602b131613c3647d07
kararAcode <63989704+kararAcode@users.noreply.github.com>
2023-08-19 14:25:45 -0600

    add locate feature
    shows weather for current location

--- App.js
+++ App.js
@@ -2,26 +2,45 @@ import { useEffect, useState } from 'react';
 import Weather from './src/components/Weather';
 import Loading from './src/components/Loading';
 
+import * as Location from 'expo-location';
+
 function App() {
   const [weatherData, setWeatherData] = useState({});
+  
 
   
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8080/current", {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-      })
-      .then((res) => res.json())
-      .then((json) => {
+
+    (async () => {
+      let { status } = await Location.getBackgroundPermissionsAsync();
+      let foundLocation = await Location.getCurrentPositionAsync({});
+
+      setTimeout(() => {
+        fetch(`http://localhost:8080/current`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
   
-        setWeatherData(json);
-      });
-    }, 3000)
+          body: JSON.stringify({
+            lat: foundLocation.coords.latitude,
+            lon: foundLocation.coords.longitude
+            
+          })
+        })
+        .then((res) => res.json())
+        .then((json) => {
+    
+          setWeatherData(json);
+        });
+      }, 1000)
+    })();
+
+    
+
+    
     
 
 
--- server.js
+++ server.js
@@ -8,53 +8,55 @@ const cors =require('cors')
 require('dotenv').config({ path: require('find-config')('.env') });
 
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 
 
 app.post('/current', async (req, res) => {
-        const cityName = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${process.env.lat}&lon=${process.env.lon}&limit=1&appid=${process.env.APIKEY}`)
-        .then(json => json.data[0].name);
-        
+    let {lat, lon} = req.body;
+    const cityName = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${process.env.APIKEY}`)
+    .then(json => json.data[0].name);
     
-        await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${process.env.lat}&lon=${process.env.lon}&exclude=minutely,alerts&appid=${process.env.APIKEY}&units=metric`)
-            .then((json) => {
-                const currentData = {
-                    high: Math.round(json.data.daily[0].temp.max),
-                    low: Math.round(json.data.daily[0].temp.min),
-                    temp: Math.round(json.data.current.temp),
-                    condition: json.data.current.weather[0].description,
-                    city: cityName
+
+    await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${process.env.APIKEY}&units=metric`)
+        .then((json) => {
+            const currentData = {
+                high: Math.round(json.data.daily[0].temp.max),
+                low: Math.round(json.data.daily[0].temp.min),
+                temp: Math.round(json.data.current.temp),
+                condition: json.data.current.weather[0].description,
+                city: cityName
+            }
+
+            const hourlyData = json.data.hourly.map((hour) => {
+                let date = new Date(hour.dt*1000);
+                return {
+                    time: date.getHours() + ":00",
+                    icon: hour.weather[0].icon,
+                    temp: Math.round(hour.temp)
                 }
-    
-                const hourlyData = json.data.hourly.map((hour) => {
-                    let date = new Date(hour.dt*1000);
-                    return {
-                        time: date.getHours() + ":00",
-                        icon: hour.weather[0].icon,
-                        temp: Math.round(hour.temp)
-                    }
-                });
-                
-                let dailyData = json.data.daily.map((day) => {
-                    let date = new Date(day.dt*1000);
-                    return {
-                        date: date.toLocaleDateString().slice(date.toLocaleDateString().length - 4),
-                        icon: day.weather[0].icon,
-                        high: Math.round(day.temp.max),
-                        low: Math.round(day.temp.min),                
-                    }
-                });
-                
-                res.json({
-                    current: currentData,
-                    daily: dailyData,
-                    hourly: hourlyData
-                });
-    
-               
             });
+            
+            let dailyData = json.data.daily.map((day) => {
+                let date = new Date(day.dt*1000);
+                return {
+                    date: date.toLocaleDateString().slice(date.toLocaleDateString().length - 4),
+                    icon: day.weather[0].icon,
+                    high: Math.round(day.temp.max),
+                    low: Math.round(day.temp.min),                
+                }
+            });
+            
+            res.json({
+                current: currentData,
+                daily: dailyData.slice(1),
+                hourly: hourlyData.slice(1)
+            });
+
+            
+        });
  
 
     
